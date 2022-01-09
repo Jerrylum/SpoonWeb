8ad9115ae796086be6363cfc767c663ba8a48951
Jerrylum <serverofjerry@gmail.com>
2022-01-09 21:02:33 +0800

    :construction: prepare for UI design

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <p id="dd">Hello World</p>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from "vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+
+import "@/assets/scss/App.scss";
+
+
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
+    // HelloWorld,
+  },
 });
 </script>
 
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,19 @@
 const path = require('path'); // eslint-disable-line @typescript-eslint/no-var-requires
 
 module.exports = {
-    pluginOptions: {
-      webpackBundleAnalyzer: {
-        reportFilename: '../analyzer/report.html'
-      }
+  pluginOptions: {
+    webpackBundleAnalyzer: {
+      reportFilename: '../analyzer/report.html'
     }
-  };
+  },
+  // chainWebpack: config => {
+  //   config.module
+  //     .rule('scss')
+  //     .test(/\.scss$/)
+  //     .use('vue-style-loader')
+  //       .loader('css-loader')
+  //       .loader('sass-loader')
+  //       .tap(options => options)
+  //       .end()
+  // }
+};
